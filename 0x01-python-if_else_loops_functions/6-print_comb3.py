6669a6908ba01975de7db2736e539333cf7d6333
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-05-31 21:35:56 +0300

    print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(9):
     for j in range(i+1, 10):
         if i == 8 and j == 9:
-            print(i, j, sep="")
+            print("{}{}".format(i, j))
         else:
-            print(i, j, sep="", end=", ")
+            print("{}{}".format(i, j), end=", ")
