d67e5c6786f26878230f9962a3d54d81bd0b50d3
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-03 00:54:01 +0300

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif li[1] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{} + {} = {}".format(a, b, add(int(li[0]), int(li[2]))))
     elif li[1] == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(int(li[0]), int(li[2]))))
     elif li[1] == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(int(li[0]), int(li[2]))))
     elif li[1] == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("{} / {} = {}".format(a, b, div(int(li[0]), int(li[2]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
