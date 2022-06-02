96128ebae7e27ba4fc7bffb6927e36bb58837db8
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-03 00:45:28 +0300

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+    li = sys.argv[1:]
+    li_len = len(li)
+    a = li[0]
+    b = li[2]
+    if li_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n1")
+    elif li[1] == "+":
+        print("{} + {} = {}\n0".format(a, b, add(a, b)))
+    elif li[1] == "-":
+        print("{} - {} = {}\n0".format(a, b, sub(a, b)))
+    elif li[1] == "+":
+        print("{} * {} = {}\n0".format(a, b, mul(a, b)))
+    elif li[1] == "+":
+        print("{} / {} = {}\n0".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /\n1")
