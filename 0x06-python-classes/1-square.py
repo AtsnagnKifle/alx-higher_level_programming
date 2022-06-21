b356d7558a7c4aed20d9909630c1cb09d011f5c0
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-22 01:10:10 +0300

    add docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+no module imported
+"""
+
 
 class Square:
+    """
+    Defines a square by private instance size
+    """
+
     def __init__(self, size):
+        """
+        size : no type/value verification
+        Private instance attribute size
+        """
         self.__size = size
