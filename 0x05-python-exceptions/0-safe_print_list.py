1f028b18874bfd4923cd1338e82aad7f909d11d4
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-21 15:09:32 +0300

    print list with exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    co = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            co += 1
+    except:
+        pass
+    print()
+    return co
