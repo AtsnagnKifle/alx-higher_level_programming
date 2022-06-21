8a2446462b6ab18345b3f766d5fb92a87ab7549f
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-21 15:54:20 +0300

    division with exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+# 1/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        n = a/b
+        result = n
+    except ZeroDivisionError:
+        pass
+    print("Inside result: {}".format(result))
+    return result
