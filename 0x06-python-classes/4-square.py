cdd1457dc4e6d0ffa5a7a6f56f57a4c7aca461c4
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-22 01:23:37 +0300

    class with setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+no module imported
+"""
+
+
+class Square:
+    """
+    defines a square by private attribute size,
+    """
+
+    def __init__(self, size=0):
+        """
+        private instance attribute
+        parameters
+        size : integer else TypeError
+        if size less than 0, raise value error
+        """
+        self.__size = size
+        try:
+            assert type(size) == int
+        except Exception as e:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        public instance method
+        return square area
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """
+        retrieve private attribute size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set private attribute
+        """
+        self.__size = value
+        try:
+            assert type(value) == int
+        except:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
