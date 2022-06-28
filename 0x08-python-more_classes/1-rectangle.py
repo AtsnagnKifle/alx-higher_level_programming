8ecc7cef54d513db13289449049425deaa0e9c06
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-28 11:05:34 +0300

    Rectangle class with setter and getter

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+    no module imported
+"""
+
+
+class Rectangle:
+    """ An Empty Rectangle Class """
+
+    def __init__(self, width=0, height=0):
+        self._height = height
+        self._width = width
+
+    @property
+    def width(self):
+        """
+            get the value of width
+        """
+        return self._width
+
+    @property
+    def height(self):
+        """
+            get the value of height
+        """
+        return self._height
+
+    @width.setter
+    def width(self, val):
+        """
+            set the value of width
+            raise TypeError if value is not an int
+            raise ValueError if value is < 0
+        """
+        if type(val) != int:
+            raise TypeError("width must be an integer")
+        elif val < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = val
+
+    @height.setter
+    def height(self, val):
+        """
+            set the value of height
+            raise TypeError if value is not an int
+            raise ValueError if value is < 0
+        """
+        if type(val) != int:
+            raise TypeError("height must be an integer")
+        elif val < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = val
