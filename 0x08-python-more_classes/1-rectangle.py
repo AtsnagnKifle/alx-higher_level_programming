4610814296dc0a90c7df4e17b4ad7f303bc0d9fc
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-28 11:22:11 +0300

    Rectangle class init fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,19 @@ class Rectangle:
     """ An Empty Rectangle Class """
 
     def __init__(self, width=0, height=0):
-        self._height = height
-        self._width = width
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = width
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = height
 
     @property
     def width(self):
