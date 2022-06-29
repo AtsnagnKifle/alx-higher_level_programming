57d79864cacf6892bce3f78b328b8ee9b9f292e2
atsnagnkifle <kifleatsnagn@gmail.com>
2022-06-29 10:55:38 +0300

    pycodestyle bare 'except' fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,11 +10,11 @@ def add_integer(a, b=98):
     """
     try:
         assert type(a) in (float, int)
-    except:
+    except Exception as e:
         raise TypeError("a must be an integer")
     try:
         assert type(b) in (float, int)
-    except:
+    except Exception as e:
         raise TypeError("b must be an integer")
     if type(a) == float:
         a = int(a)
