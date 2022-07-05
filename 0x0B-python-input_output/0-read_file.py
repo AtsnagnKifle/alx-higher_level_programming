e59066f4b1a9583e792c21c970f99cb2ba6bdb8d
atsnagnkifle <kifleatsnagn@gmail.com>
2022-07-05 06:48:48 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+    read file
+"""
+
 
 def read_file(filename=""):
+    """
+        read file
+    """
     with open(filename, mode="r", encoding="utf-8") as a_file:
-        print(a_file.read())
+        for line in a_file:
+            print(line, end="")
