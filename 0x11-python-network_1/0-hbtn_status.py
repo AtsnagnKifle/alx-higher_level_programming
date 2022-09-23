d444d06b21341e90be08f3fc6f2817c1e9efddf3
atsnagnkifle <kifleatsnagn@gmail.com>
2022-09-23 22:57:58 +0300

    What's my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    fetch https://alx-intranet.hbtn.io/status using urllib package
+"""
+import urllib.request
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    data = response.read()
+    print()
+    print("Body response:")
+    print(" - type:", type(data))
+    print(" - content:", data)
+    print(" - utf8 content:", data.decode('utf-8'))
