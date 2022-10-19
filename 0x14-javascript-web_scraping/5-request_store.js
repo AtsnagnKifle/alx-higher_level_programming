0041a3885f89731e5be473dbdd79ebb8a2355181
atsnagnkifle <kifleatsnagn@gmail.com>
2022-10-19 20:41:20 +0300

    js web scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+  gets the contents of a webpage and stores it in a file
+  */
+const myArgs = process.argv.slice(2);
+const fs = require('fs');
+const request = require('request');
+request(myArgs[0], function (error, response, body) {
+  if (!error) {
+    fs.writeFile(myArgs[1], body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
