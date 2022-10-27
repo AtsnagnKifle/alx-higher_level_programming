d42d89ce7044027186f713b1152528c36f678956
atsnagnkifle <kifleatsnagn@gmail.com>
2022-10-27 21:01:38 +0300

    update color

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+  * updates the color of the <header> to red
+*/
+window.onload = function myFunction() {
+  const elem = document.querySelector('header');
+  elem.style.color = 'red';
+};
